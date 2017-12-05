b88e6f0ea7e82c9d99f7b1abb470676535b1c2c3
DESKTOP-2LQ81IL\Paco Lopez <Fco.Lopez15@alu.uclm.es>
2017-12-05 18:53:31 +0100

    usuario sin registar 2.0

--- LaOca/src/edu/uclm/esi/tysweb/laoca/dominio/Manager.java
+++ LaOca/src/edu/uclm/esi/tysweb/laoca/dominio/Manager.java
@@ -10,21 +10,21 @@ public class Manager {
 	private ConcurrentHashMap<String, User> usuarios;
 	private ConcurrentHashMap<Integer, Partida> partidasPendientes;
 	private ConcurrentHashMap<Integer, Partida> partidasEnJuego;
-	
+
 	private Manager(){
 		this.usuarios = new ConcurrentHashMap<>();
 		this.partidasPendientes = new ConcurrentHashMap<>();
 		this.partidasEnJuego = new ConcurrentHashMap<>();
 	}
-	
+
 	private static class ManagerHolder{
 		static Manager singleton = new Manager();
 	}
-	
+
 	public static Manager get() {
 		return ManagerHolder.singleton;
 	}
-	
+
 	public int crearPartida(String nombreJugador, int numeroDeJugadores) {
 		User usuario = findUser(nombreJugador);
 		Partida partida = new Partida(usuario, numeroDeJugadores);
@@ -40,7 +40,7 @@ public class Manager {
 		}
 		return usuario;
 	}
-	
+
 	public void addJugador(String nombreJugador) throws Exception{
 		if(this.partidasPendientes.isEmpty())
 			throw new Exception("No hay hijueputas partidas pendientes. Crea una, pendejo");
@@ -55,20 +55,22 @@ public class Manager {
 	//TODO Maybe it should return the user
 	public boolean login(String email,String pwd) throws Exception{
 		User user = DAOUser.login(email,pwd);
-		String nick = user.getNick();
-		if(nick != null) {
-			usuarios.put(nick, user);
+		if(user != null) {
+			String nick = user.getNick();
+			if(nick != null) {
+				usuarios.put(nick, user);
+			}
 		}
 		return user != null;
 	}
-	
+
 	//TODO Maybe it should return the user
 	public boolean registrar(String email,String pwd,String nick) throws Exception{
 		User user = DAOUser.insert(email, pwd, nick);
 		usuarios.put(nick, user);
 		return  user != null;
 	}
-	
+
 	public boolean jugarSinRegistrar(String nick) throws Exception{
 		User user = null;
 		if (!usuarios.containsKey(nick)) {
@@ -80,18 +82,20 @@ public class Manager {
 	//TODO Maybe it should return the user
 	public boolean changePassword(String email,String pwd,String newPassword) throws Exception{
 		User user = DAOUser.login(email, pwd);
+		String nick = user.getNick();
 		if(user != null) {
 			user.changePasswordDB(newPassword);
+			usuarios.put(nick, user);
 			return true;
 		}
 		return false;
 	}
 
-	
+
 	public void logoff(String email) {
 		this.usuarios.remove(email);
 	}
-	
+
 	public JSONObject tirarDado(int idPartida, String jugador, int dado) throws Exception {
 		Partida partida=this.partidasEnJuego.get(idPartida);
 		JSONObject mensaje=partida.tirarDado(jugador, dado);
@@ -107,7 +111,7 @@ public class Manager {
 	public JSONObject getScores() throws Exception {
 		return DAOUser.getScoreList();
 	}
-	
+
 	private void terminar(Partida partida) {
 		partida.terminar();
 		partidasEnJuego.remove(partida.getId());
